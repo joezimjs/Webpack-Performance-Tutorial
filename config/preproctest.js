43299050789cb1c5dd07d23f03027bc839048ed6
Joseph Zimmerman <contact@joezimjs.com>
2017-04-12 11:25:03 -0500

    Ignored Dist folder; removed linting/testing files.

--- config/preproctest.js
+++
@@ -1,8 +0,0 @@
-let fs = require('fs')
-let path = require('path')
-let compile = require('./jest-preprocessor.js').process
-
-let filepath = path.resolve(__dirname, `../src/lame.vue`)
-let contents = fs.readFileSync(filepath, {encoding:'utf8'})
-
-console.log(compile(contents, filepath))
